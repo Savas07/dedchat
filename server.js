b8d09eb636d12590fc8d1e61e10a3a6ed5a19d5f
Savaş Altıntaş <savas_007_2000@hotmail.com>
2020-08-09 19:49:45 +0300

    chat.html, emit, broadcast

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
 
 
 // set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'PUBLIC')));
 
+//run
+io.on('connection', socket => {
 
+    socket.emit('message', 'welcome to dedchat');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
